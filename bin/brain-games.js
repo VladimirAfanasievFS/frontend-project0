08ca86cbcc87b862506df0753c985833ff6a2bce
Vova Afan <grmh9ko@gmail.com>
2020-02-19 21:44:11 +0300

    3 stage

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
+import questWhatYourName from '../src/index.js'
+
 console.log('Welcome to the Brain Games!');
+console.log(`Hi ${questWhatYourName()}!`);
+
 
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+const questWhatYourName = () => {
+const actual = readlineSync.question('May I have your name?');
+return actual;
+}
+export default questWhatYourName
+
